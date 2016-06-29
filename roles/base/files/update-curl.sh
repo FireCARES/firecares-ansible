7773bdbeef8921beb06675dad2293f9541be9a56
Tyler Garner <garnertb@gmail.com>
2016-06-29 08:43:41 -0400

    Cache the sitemap and refresh daily.

---
+++ roles/base/files/update-curl.sh
@@ -0,0 +1,17 @@
+sudo apt-get -y build-dep curl
+
+# Get latest (as of Feb 25, 2016) libcurl
+mkdir ~/curl
+cd ~/curl
+wget http://curl.haxx.se/download/curl-7.47.0.tar.bz2
+tar -xvjf curl-7.47.0.tar.bz2
+cd curl-7.47.0
+
+# The usual steps for building an app from source
+./configure
+make
+sudo make install
+
+# Resolve any issues of C-level lib
+# location caches ("shared library cache")
+sudo ldconfig
